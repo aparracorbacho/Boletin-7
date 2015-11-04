bc76d7ce9c58b9c4887541a0108795ec1950a973
MotherFoquita <MotherFoquita@192.168.0.14>
2015-11-04 22:17:47 +0100

    Ejercicio 1

---
+++ Ejercicio 1/src/ejercicio/pkg1/Ejercicio1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MotherFoquita
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float num1;
+        System.out.println("Introduce un numero");
+        Scanner num1Teclado = new Scanner(System.in);
+        num1 = num1Teclado.nextFloat();
+        if (num1>0) { 
+            System.out.println("O numero é positivo, é o: "+num1);
+        }
+              
+             
+    }
+    
+}
